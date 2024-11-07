e354a5d2d2b2f9b8f47b119b2d98a52b7178fe9c
victor <victor.fouquet@gmail.com>
2024-11-07 20:07:57 +0100

    Refine typeguards and add type matching

--- src/type-narrowing.ts
+++ src/type-narrowing.ts
@@ -10,25 +10,23 @@ export type MyKeys = A | B | C;
 
 export type EntityA = {
     a: string
-} & Brand<"a">
+}
 
 export type EntityB = {
     b: string
-} & Brand<"b">
+}
 
 export type EntityC = {
     c: string
-} & Brand<"c">
+}
 
 export type Uniq = {
   [K in A]: string;
-} & Brand<"uniq" | "a">;
+};
 
 export type Combined = AtLeastOne<{
   [K in Exclude<MyKeys, A>]: string
-}> & {
-  [__brand]: "combined" | "b" | "c";
-};
+}>;
 
 export type Operation = (
   { a: string, b?: never, c?: never } |
@@ -36,23 +34,31 @@ export type Operation = (
   { a?: never, b: string, c?: string }
 )
 
-export function isEntityA(v: any): v is EntityA {
+export function isEntityA(v: any): v is EntityA & Brand<"a"> {
     return typeof v === "object" && Object.keys(v).length === 1 && 'a' in v
 }
 
-export function isEntityB(v: any): v is EntityB {
+export function isEntityB(v: any): v is EntityB & Brand<"b"> {
     return typeof v === "object" && Object.keys(v).length === 1 && 'b' in v
 }
 
-export function isEntityC(v: any): v is EntityC {
+export function isEntityC(v: any): v is EntityC & Brand<"c"> {
     return typeof v === "object" && Object.keys(v).length === 1 && 'c' in v
 }
 
-export function isUniq(v: any): v is Uniq {
+export function matchEntityB(v: any): v is EntityB & Combined & Brand<"combined" | "b"> {
+    return typeof v === "object" && 'b' in v
+}
+
+export function matchEntityC(v: any): v is EntityC & Combined & Brand<"combined" | "c"> {
+    return typeof v === "object" && 'c' in v
+}
+
+export function isUniq(v: any): v is Uniq & Brand<"uniq" | "a"> {
   return typeof v === "object" && Object.entries(v).every(([k,v]) => k === 'a' && typeof v === 'string');
 }
 
-export function isCombined(v: any): v is Combined {
+export function isCombined(v: any): v is Combined & Brand<"combined" | "b" | "c"> {
   return typeof v === "object" && Object.entries(v).every(([k,v]) => ['b', 'c'].includes(k) && typeof v === 'string');
 }
 
