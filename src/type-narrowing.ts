817c68b1d7d3b84a4f36e5e148578d0618b0b241
victor <victor.fouquet@gmail.com>
2024-11-08 13:38:18 +0100

    Dynamic construction refinement

--- src/type-narrowing.ts
+++ src/type-narrowing.ts
@@ -1,12 +1,17 @@
 
 declare const __brand: unique symbol;
 type AtLeastOne<T, U = { [K in keyof T]: Pick<T, K> }> = Partial<T> & U[keyof U];
+
 type Brand<B> = { [__brand]: B };
 
 export type A = "a";
 export type B = "b";
 export type C = "c";
-export type MyKeys = A | B | C;
+export type D = "d";
+
+export type Exclusive = A | D;
+
+export type MyKeys = A | B | C | D;
 
 export type EntityA = {
     [K in A]: string
@@ -20,28 +25,29 @@ export type EntityC = {
     [K in C]: string
 }
 
+export type EntityD = {
+    [K in D]: string
+}
+
 export type Uniq = {
-  [K in A]: string;
-};
+  [K in Exclusive]: AtLeastOne<{
+    [U in Exclusive]: K extends U ? string : never
+  }>;
+}[Exclusive];
 
 export type Combined = AtLeastOne<{
-  [K in Exclude<MyKeys, A>]: string
+  [K in Exclude<MyKeys, Exclusive>]: string
 }>;
 
 export type Operation = {
-    [K in MyKeys]: {
-        [U in MyKeys]?: 
-        K extends U ? string :
-        K extends A ? never  :
-        U extends A ? never  :
-        string
-    }
+    [K in MyKeys]: AtLeastOne<{
+        [U in MyKeys]: 
+            K extends U ? string :
+            K extends Exclusive ? never  :
+            U extends Exclusive ? never  :
+            string
+    }>
 }[MyKeys];
-// (
-//   { a: string, b?: never, c?: never } |
-//   { a?: never, b?: string, c: string } |
-//   { a?: never, b: string, c?: string }
-// )
 
 export function isEntityA(v: any): v is EntityA & Brand<A> {
     return typeof v === "object" && Object.keys(v).length === 1 && 'a' in v
@@ -55,6 +61,10 @@ export function isEntityC(v: any): v is EntityC & Brand<C> {
     return typeof v === "object" && Object.keys(v).length === 1 && 'c' in v
 }
 
+export function isEntityD(v: any): v is EntityD & Brand<D> {
+    return typeof v === "object" && Object.keys(v).length === 1 && 'd' in v
+}
+
 export function matchEntityB(v: any): v is EntityB & Combined & Brand<"combined" | B> {
     return typeof v === "object" && 'b' in v
 }
@@ -63,8 +73,8 @@ export function matchEntityC(v: any): v is EntityC & Combined & Brand<"combined"
     return typeof v === "object" && 'c' in v
 }
 
-export function isUniq(v: any): v is Uniq & Brand<"uniq" | A> {
-  return typeof v === "object" && Object.entries(v).every(([k,v]) => k === 'a' && typeof v === 'string');
+export function isUniq(v: any): v is Uniq & Brand<"uniq" | A | D> {
+  return typeof v === "object" && Object.entries(v).every(([k,v]) => ['a', 'd'].includes(k) && typeof v === 'string');
 }
 
 export function isCombined(v: any): v is Combined & Brand<"combined" | B | C> {
