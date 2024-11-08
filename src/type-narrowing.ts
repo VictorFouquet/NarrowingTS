c4819e80e48d92b262494be3948ad9ca7ce758b6
victor <victor.fouquet@gmail.com>
2024-11-08 14:18:37 +0100

    Refactor type guards

--- src/type-narrowing.ts
+++ src/type-narrowing.ts
@@ -1,33 +1,25 @@
 
+//------------------------------------------------------------------------------------ Utils
+
 declare const __brand: unique symbol;
+type Brand<B> = { [__brand]: B };
 type AtLeastOne<T, U = { [K in keyof T]: Pick<T, K> }> = Partial<T> & U[keyof U];
 
-type Brand<B> = { [__brand]: B };
+//------------------------------------------------------------------------------------ Types
 
 export type A = "a";
 export type B = "b";
 export type C = "c";
 export type D = "d";
 
-export type ExclusiveKeys = A | D;
-
 export type Keys = A | B | C | D;
+export type ExclusiveKeys = Exclude<Keys, B | C>;
+export type CombinableKeys = Exclude<Keys, ExclusiveKeys>;
 
-export type KeyA = {
-    [K in A]: string
-}
-
-export type KeyB = {
-    [K in B]: string
-}
-
-export type KeyC = {
-    [K in C]: string
-}
-
-export type KeyD = {
-    [K in D]: string
-}
+export type KeyA = { [K in A]: string };
+export type KeyB = { [K in B]: string };
+export type KeyC = { [K in C]: string };
+export type KeyD = { [K in D]: string };
 
 export type Exclusive = {
   [K in ExclusiveKeys]: AtLeastOne<{
@@ -36,7 +28,7 @@ export type Exclusive = {
 }[ExclusiveKeys];
 
 export type Combinable = AtLeastOne<{
-  [K in Exclude<Keys, ExclusiveKeys>]: string
+  [K in CombinableKeys]: string
 }>;
 
 export type Operation = {
@@ -49,45 +41,48 @@ export type Operation = {
     }>
 }[Keys];
 
+//------------------------------------------------------------------------------------ Type Guards
+
+function containsKeysAllWithTypeString(v: object, keys: string[]): boolean {
+    return Object.entries(v).every(([k,v]) => keys.includes(k) && typeof v === 'string');
+}
+
 export function isKeyA(v: any): v is KeyA & Brand<A> {
-    return typeof v === "object" && Object.keys(v).length === 1 && 'a' in v
+    return typeof v === "object" && containsKeysAllWithTypeString(v, ["a"]);
 }
 
 export function isKeyB(v: any): v is KeyB & Brand<B> {
-    return typeof v === "object" && Object.keys(v).length === 1 && 'b' in v
+    return typeof v === "object" && containsKeysAllWithTypeString(v, ["b"]);
 }
 
 export function isKeyC(v: any): v is KeyC & Brand<C> {
-    return typeof v === "object" && Object.keys(v).length === 1 && 'c' in v
+    return typeof v === "object" && containsKeysAllWithTypeString(v, ["c"]);
 }
 
 export function isKeyD(v: any): v is KeyD & Brand<D> {
-    return typeof v === "object" && Object.keys(v).length === 1 && 'd' in v
+    return typeof v === "object" && containsKeysAllWithTypeString(v, ["d"]);
 }
 
 export function matchKeyB(v: any): v is KeyB & Combinable & Brand<"Combinable" | B> {
-    return typeof v === "object" && 'b' in v
+    return typeof v === "object" && typeof v['b'] === "string";
 }
 
 export function matchKeyC(v: any): v is KeyC & Combinable & Brand<"Combinable" | C> {
-    return typeof v === "object" && 'c' in v
+    return typeof v === "object" && typeof v['c'] === "string";
 }
 
 export function isExclusive(v: any): v is Exclusive & Brand<"Exclusive" | A | D> {
-  return typeof v === "object" && Object.entries(v).every(([k,v]) => ['a', 'd'].includes(k) && typeof v === 'string');
+  return typeof v === "object"  && containsKeysAllWithTypeString(v, ["a", "d"]);
 }
 
 export function isCombinable(v: any): v is Combinable & Brand<"Combinable" | B | C> {
-  return typeof v === "object" && Object.entries(v).every(([k,v]) => ['b', 'c'].includes(k) && typeof v === 'string');
+  return typeof v === "object" && containsKeysAllWithTypeString(v, ["b", "c"]);
 }
 
 export function isOperation(v: any): v is Operation {
-  return (
-    typeof v === "object" &&
-    (
-      Object.entries(v).every(([k,v]) => k === 'a' && typeof v === 'string') ||
-      Object.entries(v).every(([k,v]) => ['b', 'c'].includes(k) && typeof v === 'string')
-    )
-  );
+  return typeof v === "object"
+    && (
+        containsKeysAllWithTypeString(v, ["a", "d"]) ||
+        containsKeysAllWithTypeString(v, ["b", "c"])
+    );
 }
-
